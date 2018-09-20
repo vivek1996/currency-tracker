21ae15d4e511e57d3222407a37ec897b5f674179
vivek1996 <mvivek3112@gmail.com>
2018-09-20 23:44:29 +0530

    Added jquery slim

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'currency-tracker';
-  type = 'line';
-  data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      }
-    ]
-  };
-  options = {
-    responsive: true,
-    maintainAspectRatio: false
-  };
+
 }

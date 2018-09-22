36afcd5fb6b89e9ff8f5418af153c382b0a41df6
vivek1996 <mvivek3112@gmail.com>
2018-09-22 20:43:52 +0530

    api  set as Root

--- src/app/bitcoin/coin/coin.component.ts
+++ src/app/bitcoin/coin/coin.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CoindeskapiService } from '../../coindeskapi.service';
 @Component({
   selector: 'app-coin',
   templateUrl: './coin.component.html',
   styleUrls: ['./coin.component.scss']
 })
 export class CoinComponent implements OnInit {
-  constructor() {}
+  constructor(private _http: CoindeskapiService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._http.getCurrentPrice().subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
--- src/app/coindeskapi.service.ts
+++ src/app/coindeskapi.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
-import { BitcoinModule } from './bitcoin/bitcoin.module';
+// import { BitcoinModule } from './bitcoin/bitcoin.module';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: BitcoinModule
+  providedIn: 'root'
 })
 export class CoindeskapiService {
-  constructor() {}
+  private baseUrl = `https://api.coindesk.com/v1/bpi`;
+  constructor(private http: HttpClient) {}
+  getCurrentPrice(currecy = 'INR') {
+    return this.http.get(`${this.baseUrl}/currentprice/${currecy}.json`);
+  }
 }
